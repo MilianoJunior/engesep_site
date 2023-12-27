815acb042e39aa5319b99936dc126ac2224e8d7f
Miliano Fernandes de Oliveira Junior <69368192+MilianoJunior@users.noreply.github.com>
2023-12-27 13:18:23 -0300

    add slider

--- engesep/urls.py
+++ engesep/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    # static(settings.STATIC_URL)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# for url in settings:
+#     print(url)
+print('Urls app 1: ',settings.STATIC_URL)
+print('Urls app 2: ',settings.STATICFILES_DIRS)
--- engesep_site/settings.py
+++ engesep_site/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL =  '/static/'
+print('base dir: ', BASE_DIR)
+STATICFILES_DIRS = [BASE_DIR / 'static']
+print('static files dirs: ', STATICFILES_DIRS)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
