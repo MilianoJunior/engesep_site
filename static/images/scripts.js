6f6807b11692adc550d6b3849169c700d0f7be77
Miliano Fernandes de Oliveira Junior <69368192+MilianoJunior@users.noreply.github.com>
2023-12-27 18:20:14 -0300

    add layout

--- static/images/scripts.js
+++
@@ -1,9 +0,0 @@
-let currentSlide = 0;
-
-function moveSlide(step) {
-    const slides = document.querySelector('.slides');
-    const totalSlides = document.querySelectorAll('.slide').length;
-
-    currentSlide = (currentSlide + step + totalSlides) % totalSlides;
-    slides.style.transform = 'translateX(' + (-currentSlide * 100) + '%)';
-}
