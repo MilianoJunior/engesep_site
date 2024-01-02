4b38d16512f6660b841b4b397d07c510ecbb2883
Miliano Fernandes de Oliveira Junior <69368192+MilianoJunior@users.noreply.github.com>
2024-01-02 18:56:05 -0300

    add updates view

--- engesep/views.py
+++ engesep/views.py
@@ -7,15 +7,21 @@ from django.conf import settings
 
 def index(request):
     # return HttpResponse()
-    path = os.getcwd()
-    print(path)
+    # path = os.getcwd()
+    # print(path)
     print('static url: ', settings.STATIC_URL)
     print('static root: ', settings.STATIC_ROOT)
     print('static dirs: ', settings.STATICFILES_DIRS)
+    path = os.environ.get('ASSETS_DIR', os.path.join(settings.STATICFILES_DIRS[0], 'assets'))
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    slides = [s for s in os.listdir(path) if 'slide' in s]
 
     # path = os.path.join(os.getcwd(), 'engesep_site', 'static', 'assets')
-    path = os.path.join(settings.STATICFILES_DIRS[0], 'assets')
-    slides = [s for s in os.listdir(path) if 'slide' in s]
+    # path = os.path.join(settings.STATICFILES_DIRS[0], 'assets')
+    # slides = [s for s in os.listdir(path) if 'slide' in s]
     # global slides
     print('Ola: ', slides)
     print(round(928/1261,2))
@@ -24,6 +30,3 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
-
-
-# Create your views here.
