8202e86ae73d71b606fd2a6ee666f0981b3620e8
Miliano Fernandes de Oliveira Junior <69368192+MilianoJunior@users.noreply.github.com>
2024-01-02 18:47:35 -0300

    add updates views

--- engesep/views.py
+++ engesep/views.py
@@ -3,12 +3,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-
+from django.conf import settings
 
 def index(request):
     # return HttpResponse()
-    # print(os.getcwd())
-    path = os.path.join(os.getcwd(), 'engesep_site', 'static', 'assets')
+    path = os.getcwd()
+    print(path)
+    print('static url: ', settings.STATIC_URL)
+    print('static root: ', settings.STATIC_ROOT)
+    print('static dirs: ', settings.STATICFILES_DIRS)
+
+    # path = os.path.join(os.getcwd(), 'engesep_site', 'static', 'assets')
+    path = os.path.join(settings.STATICFILES_DIRS[0], 'assets')
     slides = [s for s in os.listdir(path) if 'slide' in s]
     # global slides
     print('Ola: ', slides)
