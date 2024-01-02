a737ca09bc15f4da31c00ae73e2cf1691ae369c1
Miliano Fernandes de Oliveira Junior <69368192+MilianoJunior@users.noreply.github.com>
2024-01-02 18:30:06 -0300

    add updates layout

--- engesep/views.py
+++ engesep/views.py
@@ -1,5 +1,4 @@
 import os
-
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -8,9 +7,14 @@ from django.shortcuts import render
 
 def index(request):
     # return HttpResponse()
-    print(os.getcwd())
+    # print(os.getcwd())
+    path = os.path.join(os.getcwd(), 'engesep_site', 'static', 'assets')
+    slides = [s for s in os.listdir(path) if 'slide' in s]
+    # global slides
+    print('Ola: ', slides)
+    print(round(928/1261,2))
     template = loader.get_template('index.html')
-    context = {}
+    context = {'slides': slides}
     return HttpResponse(template.render(context, request))
 
 
